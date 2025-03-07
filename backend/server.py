e5c7cd01c2c125a6c89930f1a819ed64759e3ad2
brodeeC <brodeeclontz@gmail.com>
2025-03-07 15:55:03 -0500

    added print statement

--- backend/server.py
+++ backend/server.py
@@ -23,6 +23,7 @@ def get_line(lineNum):
         # Directly decode the line_text and speaker from Unicode escapes
         line_text = row["line_text"].encode('utf-8').decode('unicode_escape')
         speaker = row["speaker"].encode('utf-8').decode('unicode_escape')
+        print(line_text)
 
         return line_text, speaker
     
