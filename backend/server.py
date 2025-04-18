87f766f42ff4ff83e8c5b59323d396345a2e1aa9
brodeeC <brodeeclontz@gmail.com>
2025-04-18 14:17:46 -0400

    how about this?

--- backend/server.py
+++ backend/server.py
@@ -437,7 +437,7 @@ def get_word(lemma_id):
             "SELECT lemma FROM lemma_data WHERE lemma_id = ? LIMIT 1", 
             (lemma_id,)
         ).fetchone()  
-        logger.debug(data['lemma'])
+        logger.debug(data)
         return [data['lemma']] if data else []
     finally:
         conn.close()
